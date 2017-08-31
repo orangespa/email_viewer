d75bcc89ef1d0302738e94c456f93f5da975dae5
hong <luu.bichhong@gmail.com>
2017-08-31 18:08:33 +0200

    column right finish

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -2,4 +2,29 @@ class EmailsController < ApplicationController
   def index
     @emails = Email.all
   end
+
+  def show
+    @email = Email.find(params[:id])
+
+    respond_to do |format|
+      format.html { redirect_to emails_url }
+      format.json { head :no_content }
+      format.js   { render :layout => false }
+    end
+  end
+
+  def destroy
+    @email = Email.find(params[:id])
+    @email.destroy
+  end
+
+private
+
+  def set_email 
+    @email = Email.find(params[:id])
+  end
+  
+  def email_params
+    params.require(:email).permit(:object, :body)
+  end
 end
