0bcdac55279cd4fa65b45a4c0d697137d2d5be1a
hong <luu.bichhong@gmail.com>
2017-08-31 14:39:08 +0200

    Partie Rails done

---
+++ db/seeds.rb
@@ -0,0 +1,12 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+Email.create! object: "Object1", body: "lorem ipsum lorem ipsum 1"
+Email.create! object: "Object2", body: "lorem ipsum lorem ipsum 2"
+Email.create! object: "Object3", body: "lorem ipsum lorem ipsum 3"
+Email.create! object: "Object4", body: "lorem ipsum lorem ipsum 4"
+Email.create! object: "Object5", body: "lorem ipsum lorem ipsum 5"
